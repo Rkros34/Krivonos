83b7ea4ab0a2af6259deca5dcd956fb91b806faf
Rkros34 <kr.roma1996@gmail.com>
2022-05-31 01:02:21 +0300

    Task2 about stars

--- src/main/java/by/it_academy/lesson2/Main.java
+++ src/main/java/by/it_academy/lesson2/Main.java
@@ -2,10 +2,6 @@ package by.it_academy.lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "Roman";
-        int age = 25;
-        float height = 1.73f;
 
-        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
     }
 }
