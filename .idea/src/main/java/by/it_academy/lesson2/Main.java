f894d9a984c94f971430e8d970e487a4fdb88ef1
Rkros34 <kr.roma1996@gmail.com>
2022-05-30 19:46:30 +0300

    Lesson 2 task 1

--- .idea/src/main/java/by/it_academy/lesson2/Main.java
+++ .idea/src/main/java/by/it_academy/lesson2/Main.java
@@ -2,6 +2,10 @@ package by.it_academy.lesson2;
 
 public class Main {
     public static void main(String[] args) {
+        String name = "Roman";
+        int age = 25;
+        float height = 1.73f;
 
+        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
     }
 }
