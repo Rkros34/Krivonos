953fc597daf989925979e15c04d7c37604e33899
Rkros34 <kr.roma1996@gmail.com>
2022-05-31 08:23:05 +0300

    Task1 about array in string

---
+++ src/main/java/by/it_academy/lesson4/Task1.java
@@ -0,0 +1,22 @@
+package by.it_academy.lesson4;
+
+/**
+ * Создать массив чисел.
+ * Напечатать числа разделенные запятой и пробелом в одну строчку.
+ * Например: для new int[]{5, 1, 3, 4, 2};
+ * напечатать 5, 1, 3, 4, 2
+ */
+public class Task1 {
+    public static void main(String[] args) {
+       int [] myArray={5,1,3,4,2};
+       for (int i=0; i<myArray.length; i++)
+       {
+           if(i==myArray.length-1){
+               System.out.print(myArray[i]);
+           }else {
+               System.out.print(myArray[i]+", ");
+           }
+
+        }
+    }
+}
