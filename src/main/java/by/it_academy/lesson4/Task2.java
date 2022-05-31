f4baf73cc5f2045ff48b2e58c9b14906544ed31a
Rkros34 <kr.roma1996@gmail.com>
2022-05-31 18:30:26 +0300

    Task about stars

---
+++ src/main/java/by/it_academy/lesson4/Task2.java
@@ -0,0 +1,22 @@
+package by.it_academy.lesson4;
+
+public class Task2 {
+    /**
+     * Создать массив с неотрицательными числами.
+     * Напечатать звезды, количеством соответствующим цифре в массиве.
+     * Например: для new int[]{1,2,3}; напечатать
+     *  *
+     *  **
+     *  ***
+     * @param args
+     */
+    public static void main(String[] args) {
+        int [] myArray={20,5,5,5,20};
+        for (int i :myArray){
+            for(int j=0; j<i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
