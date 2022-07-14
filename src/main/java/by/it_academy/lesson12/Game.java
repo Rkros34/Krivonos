62a76fae0cb7cd887f11395db4f5b0552fd2c742
Rkros34 <kr.roma1996@gmail.com>
2022-07-14 12:27:24 +0300

    Task2 about count of team's result

--- src/main/java/by/it_academy/lesson12/Game.java
+++ src/main/java/by/it_academy/lesson12/Game.java
@@ -1,27 +1,54 @@
 package by.it_academy.lesson12;
+/**
+ * Нужно написать программу, которая будет спрашивать пользователя название команды,
+ * пользователь через консоль введет название команды.
+ * После этого программа должна напечатать:
+ * общее количество игр с этой командой, количество побед и количество проигрышей.
+ * Информацию в формате 'первая команда, вторая команда, очки первой команды, очки второй команды' брать из файла data.csv
+ */
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-        readFromFile();
-    }
-
-    private static void readFromFile() {
-        Path path = Path.of("D:\\IT Academy\\data.csv");
-        try {
-            List<String> gameInfoList = Files.readAllLines(path);
-            for (String str : gameInfoList) {
-                String[] listOfGame=str.split("\\,");
-                new GameInfo(listOfGame[0],listOfGame[1], Integer.parseInt(listOfGame[2]),Integer.parseInt(listOfGame[3]));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input the team's name: ");
+        var teamFromUser = scanner.nextLine();
+        scanner.close();
+        int wins=0;
+        int loses=0;
+        for (GameInfo game:readFromFile()) {
+            if (game.hasTeam(teamFromUser)){
+                if (game.getWinner().equals(teamFromUser)){
+                    wins++;
+                }else {
+                    loses++;
+                }
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-
+        System.out.println("Total result: "+(wins+loses));
+        System.out.println("Wins: "+wins);
+        System.out.println("Loses: "+loses);
     }
+
+
+        private static ArrayList<GameInfo> readFromFile () {
+            var games = new ArrayList<GameInfo>();
+            Path path = Path.of("D:\\IT Academy\\data.csv");
+            try {
+                List<String> gameInfoList = Files.readAllLines(path);
+                for (String str : gameInfoList) {
+                    String[] listOfGame = str.split(",");
+                    games.add(new GameInfo(listOfGame[0], listOfGame[1], Integer.parseInt(listOfGame[2]), Integer.parseInt(listOfGame[3])));
+                }return games;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 }
 
--- src/main/java/by/it_academy/lesson12/GameInfo.java
+++ src/main/java/by/it_academy/lesson12/GameInfo.java
@@ -1,5 +1,17 @@
 package by.it_academy.lesson12;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Нужно написать программу, которая будет спрашивать пользователя название команды,
+ * пользователь через консоль введет название команды.
+ * После этого программа должна напечатать:
+ * общее количество игр с этой командой, количество побед и количество проигрышей.
+ * Информацию в формате 'первая команда, вторая команда, очки первой команды, очки второй команды' брать из файла data.csv
+ */
+
 public class GameInfo {
     private final String nameOfFirstTeam;
     private final String nameOfSecondTeam;
@@ -12,5 +24,18 @@ public class GameInfo {
         this.nameOfSecondTeam = nameOfSecondTeam;
         this.pointOfFirstTeam = pointOfFirstTeam;
         this.pointOfSecondTeam = pointOfSecondTeam;
+    }
+
+    boolean hasTeam (String teamFromUser){
+        return nameOfFirstTeam.equals(teamFromUser)||nameOfSecondTeam.equals(teamFromUser);
+    }
+
+    String getWinner(){
+        if(pointOfFirstTeam>pointOfSecondTeam){
+            return nameOfFirstTeam;
+        }if (pointOfSecondTeam>pointOfFirstTeam){
+            return nameOfSecondTeam;
+        }
+        return "dead heat";
     }
 }
