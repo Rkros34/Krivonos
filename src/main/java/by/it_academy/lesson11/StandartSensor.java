e8e83e233a2a74d3acd6bc9aa27fdc966a02e70f
Rkros34 <kr.roma1996@gmail.com>
2022-07-19 18:13:22 +0300

    Task2 about prime numbers in range from user corrected

--- src/main/java/by/it_academy/lesson11/StandartSensor.java
+++ src/main/java/by/it_academy/lesson11/StandartSensor.java
@@ -1,10 +1,12 @@
 package by.it_academy.lesson11;
 
-public class StandartSensor implements Sensor{
+public class StandartSensor implements Sensor {
     private int sensorStatus;
-    public StandartSensor(){
-        this.sensorStatus=sensorStatus;
+
+    public StandartSensor(int sensorStatus) {
+        this.sensorStatus = sensorStatus;
     }
+
     @Override
     public boolean isOn() {
         return true;
