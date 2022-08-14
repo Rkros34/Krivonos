1a83d61aac86dc46d4cf471397a79df857bf44fc
Rkros34 <kr.roma1996@gmail.com>
2022-08-14 22:11:53 +0300

    GameEnvironment

--- src/main/java/by/it_academy/lesson18/gamingenvironment/KittiesAndPuzzles.java
+++ src/main/java/by/it_academy/lesson18/gamingenvironment/KittiesAndPuzzles.java
@@ -3,11 +3,11 @@ package by.it_academy.lesson18.gamingenvironment;
 public class KittiesAndPuzzles implements GameElementFactory {
     @Override
     public Player player() {
-        return null;
+        return new Kitty();
     }
 
     @Override
     public Obstacle obstacle() {
-        return null;
+        return new Puzzle();
     }
 }
